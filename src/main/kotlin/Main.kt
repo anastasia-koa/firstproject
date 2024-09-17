60dbb7313c8f7d6bc215acd4996a8b18e7e1cbcd
10221234 <komarova10102006@gmail.com>
2024-09-17 20:16:42 +0300

    копировали строки

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,7 @@
 fun main() {
     val valOne = 5
     val valTwo = 14
-    val valThree = 8
     println("Произведение 5*14= ${valOne*valTwo}")
     println("Произведение 5*14= ${valOne*valTwo}")
-    println("Произведение 5*14= ${valOne*valThree}")
+    println("Произведение 5*14= ${valOne*valTwo}")
 }
